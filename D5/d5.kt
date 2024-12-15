61276cc07aaa2163472b53b106da77910e51394b
oldbay <lancastermikael1994@gmail.com>
2024-12-15 02:05:55 -0500

    Comments and linting.

--- D5/d5.kt
+++ D5/d5.kt
@@ -1,20 +1,21 @@
 import java.io.File
-import java.io.IOException
 
 fun main() {
+  // read and assign the rules input file
   val rulesLines = File("rules.txt").readLines();
   val updatesLines = File("updates.txt").readLines();
-
-  val rules = mutableMapOf<Int, MutableList<Int>>();
-  val updates = mutableListOf<MutableList<Int>>();
-
   for(line in rulesLines) {
     val (x, y) = line.split("|").map { it.toInt() }
     rules.getOrPut(x) { mutableListOf() }.add(y);
   }
+
+  // read and assign the updates input file
+  val rules = mutableMapOf<Int, MutableList<Int>>();
+  val updates = mutableListOf<MutableList<Int>>();
   for(line in updatesLines) {
     updates.add(line.split(",").map { it.toInt() }.toMutableList());
   }
+
 
   fun isSorted(update: List<Int>): Boolean {
     for (i in 1 until update.size) {
@@ -29,16 +30,17 @@ fun main() {
 
   fun sortUpdate(update: MutableList<Int>) {
     update.sortWith { a, b ->
-      rules[a]?.contains(b)?.let { if (it) -1 else 1 } ?: 0
-    }
+      rules[a]?.contains(b)?.let { if (it) -1 else 1 } ?: 0 }
   }
 
-  fun middlePage(update: List<Int>): Int = update[update.size / 2]
+  fun middlePage(update: List<Int>): Int = update[update.size / 2];
 
-  val sortedUpdates = updates.filter(::isSorted)
-  println("Sum of middle pages: ${sortedUpdates.sumOf(::middlePage)}")
+  val sortedUpdates = updates.filter(::isSorted);
+  println("Middle page number sum: ${sortedUpdates.sumOf(::middlePage)}");
 
-  val unsortedUpdates = updates.filterNot(::isSorted)
-  unsortedUpdates.forEach(::sortUpdate)
-  println("Sum of middle pages (after sorting): ${unsortedUpdates.sumOf(::middlePage)}")
+  val unsortedUpdates = updates.filterNot(::isSorted);
+  unsortedUpdates.forEach(::sortUpdate);
+  println(
+    "Sorted middle page number sum: ${unsortedUpdates.sumOf(::middlePage)}"
+  );
 }
